4da864670f2b9c33cc64b66918119cd2ca64be69
evgeniy.satushev <evgeniysatushev@gmail.com>
2024-01-23 15:17:44 +0300

    Add Differ.java, Update App.java, Makefile

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,36 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.Callable;
+
+import static picocli.CommandLine.*;
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference")
+public class App implements Callable<Integer> {
+    @Parameters(index = "0", description = "path to first file")
+    String filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    String filepath2;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    String format;
+
+    @Override
+    public Integer call() {
+        try {
+            Path firstPath = Paths.get(filepath1).toAbsolutePath().normalize();
+            Path seconfPath = Paths.get(filepath2).toAbsolutePath().normalize();
+            String diffBetweenJson = Differ.generate(Differ.getData(firstPath), Differ.getData(seconfPath));
+            System.out.println(diffBetweenJson);
+        } catch (IOException e) {
+            System.out.println("THE FILE DOES NOT EXIST IN THE SPECIFIED DIRECTORY \n" + e);
+        }
+        return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("test launch");
+        System.exit(new CommandLine(new App()).execute(args));
     }
 }
