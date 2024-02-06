0b1b5682787e9ef1628c2accb527fa242aa01c71
mahoney <evgeniysatushev@gmail.com>
2024-02-06 20:46:27 +0300

    Refactor Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -10,6 +10,9 @@ import java.util.Map;
 
 
 public class Differ {
+    public static String generate(String filepath1, String filepath2) throws IOException {
+        return generate(filepath1, filepath2, "stylish");
+    }
     public static String generate(String filepath1, String filepath2, String format)
             throws IOException {
         Map<String, Object> firstFile = Parser.getData(getFilePath(filepath1));
